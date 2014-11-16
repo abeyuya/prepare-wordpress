874c429f1ce32d3f49560a6e9bd5cf9ab9f7b4c0
abeyuya <yuya.abe.0525@gmail.com>
2014-11-16 12:17:23 +0900

    first commit

---
+++ CHANGELOG.md
@@ -0,0 +1,13 @@
+setup_wordpress CHANGELOG
+=========================
+
+This file is used to list changes made in each version of the setup_wordpress cookbook.
+
+0.1.0
+-----
+- [your_name] - Initial release of setup_wordpress
+
+- - -
+Check the [Markdown Syntax Guide](http://daringfireball.net/projects/markdown/syntax) for help with Markdown.
+
+The [Github Flavored Markdown page](http://github.github.com/github-flavored-markdown/) describes the differences between markdown on github and standard markdown.
---
+++ recipes/default.rb
@@ -0,0 +1,41 @@
+#
+# Cookbook Name:: setup_wordpress
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+####################
+# wordpress link
+####################
+
+directory node["wordpress"]["install_path"] do
+  owner  'apache'
+  group  'apache'
+  mode   '0755'
+  action :create
+end
+
+link "#{node["wordpress"]["install_path"]}/#{node["wordpress"]["project_name"]}" do
+  owner  'apache'
+  group  'apache'
+  to "#{node["wordpress"]["project_paleced_dir"]}/#{node["wordpress"]["project_name"]}"
+  link_type :symbolic
+end
+
+
+############################
+# create wordpress database
+############################
+execute 'create wordpress db' do
+  user  'apache'
+  group 'apache'
+  command  <<-EOH
+    mysql -uroot -e "create database #{node["wordpress"]["wp_db_name"]}"
+    mysql -uroot -e "grant all privileges on #{node["wordpress"]["wp_db_name"]}.* to #{node["wordpress"]["wp_db_user_name"]}@localhost identified by '#{node["wordpress"]["wp_db_password"]}'"
+    mysql -uroot -e "flush privileges"
+  EOH
+  action :run
+end
